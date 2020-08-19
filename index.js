dd1f2da01bd32b8f4c4ff0eedeb312a493593788
Joachim <j1do@ucsd.edu>
2020-08-19 12:46:32 -0700

    implemented embeds

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const { Client, MessageEmbed, DiscordAPIError } = require('discord.js');
+const { Client, MessageEmbed, DiscordAPIError, Channel } = require('discord.js');
 const fetch = require('node-fetch');
 const client = new Client();
 const prefix = '!';
 
-const k = 'RGAPI-f7b139d8-6817-45d8-a13a-3b6909da6332';
+const k = 'RGAPI-ca94eb1e-b923-4d97-b35d-5aa174158d23';
 
 let players = new Map();
+let tierPieces = new Map();
+tierPieces.set('IRON', 'https://i.imgur.com/pmkjo8T.png');
+tierPieces.set('BRONZE', 'https://i.imgur.com/KnJMWka.png');
+tierPieces.set('SILVER', 'https://i.imgur.com/fnLbprT.png');
+tierPieces.set('GOLD', 'https://i.imgur.com/qdcHMVp.png');
+tierPieces.set('PLATINUM', 'https://i.imgur.com/xR2R4mt.png');
+tierPieces.set('DIAMOND', 'https://i.imgur.com/VG4Urmj.png');
+tierPieces.set('MASTERS', 'https://i.imgur.com/HirESXG.png');
+tierPieces.set('GRANDMASTER', 'https://i.imgur.com/Go0Ou9X.png');
+tierPieces.set('CHALLENGER', 'https://i.imgur.com/mt3apd6.png');
 
 client.once('ready', () => {
 	console.log('rd');
@@ -21,11 +31,11 @@ client.on('message', async message => {
     if (command === 'add') {
         const ign = args.join(' ');
         const info = await fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${ign}?api_key=${k}`).then(response => response.json());
-        players.set(ign, info.id);
+        players.set(ign, {id: info.id, profileIconId: info.profileIconId});
     }
 
     if (command === 'delete') {
-        const ign = args.join(' ');
+        const ign = args.join(' ').toLowerCase();
         if (players.has(ign)) {
             players.delete(ign);
         }
@@ -36,46 +46,33 @@ client.on('message', async message => {
     if (command === 'ranks') {
         console.log(players);
         players.forEach( async (value, key) => {
-            const info = await fetch(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${value}?api_key=${k}`).then(response => response.json());
+            const info = await fetch(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${value.id}?api_key=${k}`).then(response => response.json());
             let soloq = {};
             for (const element of info) {
                 if (element.queueType === 'RANKED_SOLO_5x5') {
                     soloq = element;
                     break;
                 }
-<<<<<<< HEAD
             }
-            const {tier, rank, summonerName, leaguePoints} = soloq;
-            const str = `${summonerName} is ${tier} ${rank} ${leaguePoints.toString()} lp.`;
-            message.channel.send(str);
-            
-=======
-			}
         const {tier, rank, summonerName, leaguePoints} = soloq;
-		const str = `${summonerName} is ${tier} ${rank} ${leaguePoints.toString()} lp.`;
-		message.channel.send(str);
-		tierImage(tier);
->>>>>>> f99fddcb6b101375f7f07b1bc3f1f211663df5a5
+        const profIcon = players.get(summonerName.toLowerCase()).profileIconId;
+		const str = `${tier} ${rank} ${leaguePoints.toString()} lp`;
+        const embed = new MessageEmbed()
+            .setColor('#00c3ff')
+            .setTitle(str)
+            .setAuthor(summonerName,
+             `http://ddragon.leagueoflegends.com/cdn/10.16.1/img/profileicon/${profIcon}.png`,
+             `https://na.op.gg/summoner/userName=${summonerName}`)
+            .setDescription(`https://na.op.gg/summoner/userName=${summonerName}`)
+            .setImage(tierPieces.get(tier));
+
+        message.channel.send(embed);
         });
 	}
 
-	async function tierImage(str)
-	{
-		let tierPieces = new Map();
-		tierPieces.set('IRON', 'https://i.imgur.com/pmkjo8T.png');
-		tierPieces.set('BRONZE', 'https://i.imgur.com/KnJMWka.png');
-		tierPieces.set('SILVER', 'https://i.imgur.com/fnLbprT.png');
-		tierPieces.set('GOLD', 'https://i.imgur.com/qdcHMVp.png');
-		tierPieces.set('PLATINUM', 'https://i.imgur.com/xR2R4mt.png');
-		tierPieces.set('DIAMOND', 'https://i.imgur.com/VG4Urmj.png');
-		tierPieces.set('MASTERS', 'https://i.imgur.com/HirESXG.png');
-		tierPieces.set('GRANDMASTER', 'https://i.imgur.com/Go0Ou9X.png');
-		tierPieces.set('CHALLENGER', 'https://i.imgur.com/mt3apd6.png');
-
-		message.channel.send(tierPieces.get(str));
-	}
+	
 
 });
 
 
-client.login('');
+client.login('NzQzNjM3NzYxNTg3ODA2MzY4.XzXkog.txE08owJFPALoVWtBWdHTJ2IwmA');
