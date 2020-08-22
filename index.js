18be0171d15357b7e6e0d9fd2df20e5872b0e979
Joachim <j1do@ucsd.edu>
2020-08-21 17:16:34 -0700

    changed help command

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import help from "./commands/help.js";
+
 const { Client, MessageEmbed, DiscordAPIError, Channel } = require('discord.js');
 const dotenv = require('dotenv');
 const fetch = require('node-fetch');
+const { default: help } = require('./commands/help');
 const client = new Client();
 const prefix = '$';
 
@@ -36,12 +39,7 @@ client.on('message', async message => {
     }
 
     if (command === 'help') {
-        const str = 
-            "$add [summoner name] Adds a player to the list." +
-            "\n$delete [summoner name] Deletes a player from the list." +
-            "\n$ranks Lists all added players and their ranks." + 
-            "\n$rank [summoner name] Shows only the given summoner's rank."
-        message.channel.send(str);
+        help();
     }
 
 
