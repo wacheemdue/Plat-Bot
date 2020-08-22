880f12c93d1e2aa9a26eff15176006ffa75ad5e8
Joachim <j1do@ucsd.edu>
2020-08-21 20:16:15 -0700

    debugged unranked players

--- commands/delete.js
+++ commands/delete.js
@@ -2,9 +2,9 @@ module.exports = function (message, global, args) {
     const ign = args.join('').toLowerCase();
     if (global[message.guild.id].has(ign)) {
         global[message.guild.id].delete(ign);
-        message.channel.send('Successfully deleted ' + ign + ".");
+        message.channel.send('Successfully deleted!");
     }
     else {
-        message.channel.send('ERROR: player ' + ign + ' does not exist.');
+        message.channel.send('ERROR: player not in list.');
     }
 }
