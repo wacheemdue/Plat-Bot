085c4a2b5aa47393a79d2c1197c75c58ea7644d6
Joachim <j1do@ucsd.edu>
2020-08-21 17:06:41 -0700

    changed help command

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ client.on('message', async message => {
         const str = 
             "$add [summoner name] Adds a player to the list." +
             "\n$delete [summoner name] Deletes a player from the list." +
-            "\n$ranks Lists all added players and their ranks."
+            "\n$ranks Lists all added players and their ranks." + 
+            "\n$rank [summoner name] Shows only the given summoner's rank."
         message.channel.send(str);
     }
 
